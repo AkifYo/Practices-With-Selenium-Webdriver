5b254639bc4ac68949f72b4b6a52b81c66a3f2e3
akif <70863898+akif-ned@users.noreply.github.com>
2022-12-01 10:23:59 +0100

    last changes

--- src/test/java/P03_Ui_UseMethods/A_Run_Class.java
+++ src/test/java/P03_Ui_UseMethods/A_Run_Class.java
@@ -5,8 +5,10 @@ public class A_Run_Class {
         B_FirstMethods.setUp();
         B_FirstMethods.cookiesHandle();
         C_Search_Box.searchText("google");
-        D_Price.priceControl("90","1500");
-        E_Review.chooseStars(5);
+        //D_Price.priceControl("100","1500");
+       E_Review.chooseStars(5);
+        Thread.sleep(5000);
+        F_Products_Title.firstPageProducts();
         B_FirstMethods.finishTest();
     }
 
--- src/test/java/P03_Ui_UseMethods/E_Review.java
+++ src/test/java/P03_Ui_UseMethods/E_Review.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class E_Review {
+
+
     public static void chooseStars(int starValue){
         WebElement starsFilter1= B_FirstMethods.driver.findElement(By.cssSelector("input[value='"+starValue+"']"));
 //        WebElement starsFilter2= B_FirstMethods.driver.findElement(By.cssSelector("input[value='2']"));
