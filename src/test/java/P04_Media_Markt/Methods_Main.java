d9ffef1be38a090c8dd963fe954d8813a838ca45
akif <70863898+akif-ned@users.noreply.github.com>
2022-12-06 22:13:14 +0100

    last changes

--- src/test/java/P04_Media_Markt/Methods_Main.java
+++ src/test/java/P04_Media_Markt/Methods_Main.java
@@ -21,13 +21,28 @@ public class Methods_Main {
         driver.findElement(By.xpath("//button[@id='pwa-consent-layer-accept-all-button']")).click();
         Thread.sleep(3000);
         List<WebElement> navbarBtns = driver.findElements(By.cssSelector("span[class='BaseTypo-sc-1jga2g7-0 izkVco StyledInfoTypo-sc-1jga2g7-1 fMeBkU Text-sc-1drhx1h-4 gruBya']"));
+        //WebElement cookiBtn=driver.findElement(By.cssSelector("button[class='btn btn-primary']"));
+        //for (WebElement each : navbarBtns) {
 
-        for (WebElement each : navbarBtns) {
-            each.click();
+            //each.click();
+            //Thread.sleep(2000);
+            //if ( each.getText().equalsIgnoreCase("outlet")){
+               //cookiBtn.click();
+            //}
+
+
+
+            //driver.navigate().back();
+            //Thread.sleep(2000);
+        //}
+        Thread.sleep(2000);
+        List<WebElement> homeCategoriesBtn= driver.findElements(By.cssSelector("div[class='StyledCell-sc-1wk5bje-0 hA-dYlB']"));
+        for (int i = 0; i < homeCategoriesBtn.size(); i++) {
+
+            homeCategoriesBtn.get(i).click();
             Thread.sleep(2000);
             driver.navigate().back();
             Thread.sleep(2000);
-
         }
     }
 
