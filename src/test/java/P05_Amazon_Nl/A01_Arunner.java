5b6298c0f189c5940c10680e3bd53ed1de48e618
akif <70863898+akif-ned@users.noreply.github.com>
2022-12-16 20:47:27 +0100

    last commit

---
+++ src/test/java/P05_Amazon_Nl/A01_Arunner.java
@@ -0,0 +1,9 @@
+package P05_Amazon_Nl;
+
+public class A01_Arunner {
+    public static void main(String[] args) throws InterruptedException {
+        A01_SetUp.setUp();
+        A01_SetUp.cookiesHandle();
+        A01_SetUp.finishTest();
+    }
+}
---
+++ src/test/java/P05_Amazon_Nl/A01_SetUp.java
@@ -0,0 +1,27 @@
+package P05_Amazon_Nl;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class A01_SetUp {
+    static WebDriver driver;
+
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.amazon.nl/");
+    }
+    public static void cookiesHandle(){
+        driver.findElement(By.id("sp-cc-accept")).click();
+    }
+
+    public static void finishTest() throws InterruptedException {
+        Thread.sleep(8000);
+        driver.quit();
+    }
+}
