4fb7886819067ae8191b453b922f1d2c60da1513
akif <70863898+akif-ned@users.noreply.github.com>
2023-01-01 23:28:37 +0100

    last commit

--- src/test/java/P10_Facebook_FormFilling/A02_Walmart_Send_keys.java
+++ src/test/java/P10_Facebook_FormFilling/A02_Walmart_Send_keys.java
@@ -23,11 +23,17 @@ public class A02_Walmart_Send_keys extends TestBase {
         Actions actoins= new Actions(driver);
         actoins.sendKeys(srcBx, "ring of the lords").perform();
         WebDriverWait waitNw= new WebDriverWait(driver, Duration.ofSeconds(15));
-
+        //e71c9f7cb5f9cbf7ed7c1cb4ad0f78dd9709dff4970633409f78a6a2aef429490c0cc1979719e0dba252899b1f39c0047a9d0f622024424b54aa60586ff0f871
+        //fbf8138af6a9ca17ea5e24506a8b9c8d9cd9f9ae0ad310832e874a1896fa8e06963b6b2bca1acf12bd3bf58efad0a8f06adca115eba6caff3202efb4b243f4b5
        // WebElement holdBtn=waitNw.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//p[@id='uNZerhzNfTvnEcx'])[9]")));        //waitNw.withTimeout(Duration.ofSeconds(15)).until(holdBtn,driver)
        //WebElement holdBtn= driver.findElement(By.xpath("//div[@aria-label='Press & Hold']"));
+
+      WebElement ifEl= driver.findElement(By.xpath("//div[@id='px-captcha']"));
+      //WebElement holdBtn=driver.switchTo().frame(ifEl).findElement(By.xpath("//div[@aria-label='Press & Hold']"));
+
         ReuseableMethod.wait(10);
-       //actoins.moveToElement(holdBtn).clickAndHold().perform();
-        ReuseableMethod.wait(300);
+     actoins.clickAndHold(ifEl); //*[@id="px-captcha"]//iframe[6]
+        ReuseableMethod.wait(3);
+        //This code failed to reach to mainwebpage
     }
 }
