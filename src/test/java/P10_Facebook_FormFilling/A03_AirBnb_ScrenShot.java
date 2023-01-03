d9f9852e61d514da7bc60e36ff5bb1cf1fa89b30
akif <70863898+akif-ned@users.noreply.github.com>
2023-01-03 22:20:48 +0100

    Last examples

---
+++ src/test/java/P10_Facebook_FormFilling/A03_AirBnb_ScrenShot.java
@@ -0,0 +1,42 @@
+package P10_Facebook_FormFilling;
+
+import Utilities.TestBase;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class A03_AirBnb_ScrenShot extends TestBase {
+    @Test
+    public void test01() throws InterruptedException {
+        // navigate to ainbnb.com
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        driver.get("https://www.airbnb.com");
+        // locate the search box element end initialize it to a webelement
+        WebElement srcBx= driver.findElement(By.xpath("//span[@id='littleSearchLabel']"));
+        Thread.sleep(2000);
+        //srcBx.click();
+        // now activate the search button to enter a place to go
+        Actions actions= new Actions(driver);
+        WebElement srcBx2= driver.findElement(By.xpath("//input[@id='bigsearch-query-location-input']"));
+        Thread.sleep(1000);
+        actions.moveToElement(srcBx2).click()
+                .perform();
+        Thread.sleep(1000);
+        srcBx2.sendKeys("Istanbul");
+        srcBx2.submit();
+        //get the title of the first four places to go
+        Thread.sleep(3000);
+        driver.quit();
+
+
+    }
+}
