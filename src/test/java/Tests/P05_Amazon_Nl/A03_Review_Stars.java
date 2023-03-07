111b7222fe52ea49b8c5d2b6877afd2b09488e72
akif <70863898+akif-ned@users.noreply.github.com>
2023-03-07 23:18:40 +0100

    last

--- src/test/java/Tests/P05_Amazon_Nl/A03_Review_Stars.java
+++ src/test/java/Tests/P05_Amazon_Nl/A03_Review_Stars.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 
 public class A03_Review_Stars {
     public static void reviewStar(int reviewEndMore){
-       WebElement stars= A01_SetUp.driver.findElement(By.xpath("//i[@class='a-icon a-icon-star-medium a-star-medium-"+reviewEndMore+"']"));
-        stars.click();
+       WebElement reviewStars= A01_SetUp.driver.findElement(By.xpath("//i[@class='a-icon a-icon-star-medium a-star-medium-"+reviewEndMore+"']"));
+        reviewStars.click();
     }
 }
